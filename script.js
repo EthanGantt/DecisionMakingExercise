5bb6b213423f31ff403f662e581a03da748a8cff
Ethan <ethan.gantt@stu.barren.kyschools.us>
2024-02-07 10:07:31 -0600

    Finished Exercise

---
+++ script.js
@@ -0,0 +1,32 @@
+let randomNum = Math.random()
+randomNum = randomNum * 5
+randomNum = Math.floor(randomNum)
+randomNum++;
+console.log(randomNum);
+
+if (randomNum >= 4){
+    console.log("Greater than or equal to 4");
+}else if (randomNum === 2){
+}else if (randomNum === 3){
+    console.log("Equal to 2 or 3");
+}else {
+    console.log("Equal to 1");
+}
+
+if (randomNum != 3){
+    console.log("Not equal to 3");
+}
+
+if (randomNum === 1){
+    console.log("NOT equal to 3 AND not equal to 5" );
+} else if (randomNum === 2){
+    console.log("NOT equal to 3 AND not equal to 5" );
+} else if (randomNum === 4){
+    console.log("NOT equal to 3 AND not equal to 5" );
+}
+
+if (randomNum === 2){
+    console.log("Equal to 2 OR equal to 4");
+} else if (randomNum === 4){
+    console.log("Equal to 2 OR equal to 4");
+}
